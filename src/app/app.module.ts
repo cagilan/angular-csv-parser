f805fb20fdee0e82fa68fd5e83ef1e79ba339ca5
vc27xo <chandran.agilan@ing.com>
2020-04-26 19:23:19 +0200

    used angular material for UI design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+// import { NgxSpinnerModule } from 'ngx-spinner';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MaterialFileInputModule } from 'ngx-material-file-input';
+import { ReactiveFormsModule } from '@angular/forms';
+// import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParserComponent } from './parser/parser.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,8 +31,18 @@ import { ParserComponent } from './parser/parser.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxSpinnerModule,
-    FormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MaterialFileInputModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/parser/parser.component.ts
+++ src/app/parser/parser.component.ts
@@ -1,7 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Papa } from 'ngx-papaparse';
 import { IssueCountFilterService } from '../services/issue-count-filter.service';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FileInput } from 'ngx-material-file-input';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 
 @Component({
   selector: 'app-parser',
@@ -25,14 +52,26 @@ export class ParserComponent implements OnInit {
   sortedInfo: any[] = [];
   headers: any[] = [];
   currentActiveSortIndex: string;
+  showSpinner = false;
+  form: FormGroup;
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   /**
    *
    * @param csvParse Object of PapaParser
    * @param filterService Object of IssueCountFilterService
    */
-  constructor(private csvParse: Papa, private filterService: IssueCountFilterService, private spinner: NgxSpinnerService) { }
-
+  constructor(private csvParse: Papa,
+              private filterService: IssueCountFilterService,
+              private fb: FormBuilder,
+              iconRegistry: MatIconRegistry,
+              sanitizer: DomSanitizer) {
+      iconRegistry.addSvgIcon(
+        'open_in_browser',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/open_in_browser-black-48dp.svg'));
+     }
   /**
    *
    * @param uploadEvent : File Object
@@ -40,7 +79,7 @@ export class ParserComponent implements OnInit {
   onChange(uploadEvent: any) {
 
     if (uploadEvent[0]) {
-      this.spinner.show();
+      this.showSpinner = true;
       this.csvParse.parse(uploadEvent[0], {
         header: true,
         skipEmptyLines: true,
@@ -61,7 +100,7 @@ export class ParserComponent implements OnInit {
     this.userInfoToShow = result.data;
     setTimeout(() => {
       /** spinner ends after 500 milli-seconds */
-      this.spinner.hide();
+      this.showSpinner = false;
     }, 500);
   }
 
@@ -74,6 +113,11 @@ export class ParserComponent implements OnInit {
     if (this.userInfoToShow.length === 0 && this.selectedIssueCount === '') {
       this.userInfoToShow = this.userInfoList;
     }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   compare = (index) => (a, b) => {
@@ -92,6 +136,10 @@ export class ParserComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
+    this.form = this.fb.group({
+      file: []
+  });
+}
+
 
 }
